89eba87bbb46268e2b05bb73d92f940df2c891b2
Ilya Kiselev <ikiselev1989@gmail.com>
2023-10-03 01:24:26 +0500

    MVP

--- src/components/GameOver.ts
+++ src/components/GameOver.ts
@@ -12,10 +12,30 @@ export default class GameOver extends Actor {
 		});
 	}
 
-	onInitialize() {
+	async onInitialize() {
 		this.addGraphics();
 
-		this.actions.moveBy(vec(0, 2200), config.character.speed);
+		await this.actions.moveBy(vec(0, 2200), config.character.speed).toPromise();
+
+		await game.waitFor(700);
+
+		this.graphics.layers
+			.create({
+				name: 'thanks',
+				order: 0,
+				offset: vec(0, -2000),
+			})
+			.use(<Sprite>res.assets.getFrameSprite('graphics/thanks'));
+
+		await game.waitFor(700);
+
+		this.graphics.layers
+			.create({
+				name: 'R',
+				order: 0,
+				offset: vec(700, -1600),
+			})
+			.use(<Sprite>res.assets.getFrameSprite('graphics/R'));
 	}
 
 	private addGraphics() {
@@ -25,12 +45,5 @@ export default class GameOver extends Actor {
 				order: 0,
 			})
 			.use(<Sprite>res.assets.getFrameSprite('graphics/final'));
-
-		this.graphics.layers
-			.create({
-				name: 'thanks',
-				order: 0,
-			})
-			.use(<Sprite>res.assets.getFrameSprite('graphics/thanks'));
 	}
 }
